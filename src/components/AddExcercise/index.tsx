c3a3e93bb86ba629d0932959f56405cf2d0af65b
LaupWing <loc_strict@hotmail.com>
2022-06-25 19:01:09 +0200

    updated

--- src/components/AddExcercise/index.tsx
+++ src/components/AddExcercise/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Modal from 'react-modal'
+import {HiOutlineMinus, HiPlus} from 'react-icons/hi'
 
 const AddExcercise = () => {
    return (
@@ -11,18 +12,27 @@ const AddExcercise = () => {
          <header className='py-1.5 bg-secundair font-bold text-sm uppercase text-white px-2'>
             <h2>Add excercise</h2>
          </header>
-         <main className='p-2 space-y-2'>
+         <main className='p-2 space-y-4 my-4'>
             <input 
                type="text" 
-               className='w-full border-2 px-2 border-secundair rounded'
+               className='w-full border-2 px-2 py-1 border-secundair rounded'
                placeholder='Oefening'
             />
-            <div className='flex'>
-               <input 
-                  type="number" 
-                  placeholder='1'
-                  className='border-2 rounded w-14 text-center border-secundair'
-               />
+            <div className='flex items-end space-x-2'>
+               <button className='w-6 h-6 border-2 flex items-center justify-center text-denied border-denied rounded-full'>
+                  <HiOutlineMinus/>
+               </button>
+               <div className='flex flex-col mt-auto'>
+                  <p className='font-bold bg-secundair rounded-t text-white text-center text-xs'>KG</p>
+                  <input 
+                     type="number" 
+                     placeholder='1'
+                     className='border-2 rounded-b w-14 text-center border-secundair'
+                  />
+               </div>
+               <button className='w-6 h-6 border-2 flex items-center justify-center text-accept border-accept rounded-full'>
+                  <HiPlus/>
+               </button>
             </div>
          </main>
       </Modal>
