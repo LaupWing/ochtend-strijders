ff94c2ea2270919eb8c6dd2e005d7555bd611c11
LaupWing <loc_strict@hotmail.com>
2022-06-25 19:18:35 +0200

    updated

--- src/components/AddExcercise/index.tsx
+++ src/components/AddExcercise/index.tsx
@@ -9,30 +9,35 @@ const AddExcercise = () => {
          overlayClassName='bg-main inset-0 fixed bg-opacity-80'
          className={`m-2 mt-10 bg-white rounded shadow overflow-hidden`}
       >
-         <header className='py-1.5 bg-secundair font-bold text-sm uppercase text-white px-2'>
+         <header className='py-1.5 bg-secundair font-bold uppercase text-white px-2'>
             <h2>Add excercise</h2>
          </header>
          <main className='p-2 space-y-4 my-4'>
-            <input 
-               type="text" 
-               className='w-full border-2 px-2 py-1 border-secundair rounded'
-               placeholder='Oefening'
-            />
+            <div className='w-full flex flex-col'>
+               <p className='font-bold bg-secundair text-white text-center text-xs mr-auto px-4 rounded-t uppercase'>Oefeningen</p>
+               <input 
+                  type="text" 
+                  className='w-full border-2 px-2 py-1 border-secundair rounded rounded-tl-none'
+                  placeholder='Oefening'
+               />
+            </div>
             <div className='flex items-end space-x-2'>
-               <button className='w-6 h-6 border-2 flex items-center justify-center text-denied border-denied rounded-full'>
-                  <HiOutlineMinus/>
-               </button>
-               <div className='flex flex-col mt-auto'>
-                  <p className='font-bold bg-secundair rounded-t text-white text-center text-xs'>KG</p>
-                  <input 
-                     type="number" 
-                     placeholder='1'
-                     className='border-2 rounded-b w-14 text-center border-secundair'
-                  />
+               <div className='flex flex-col mt-auto border-2 border-secundair rounded'>
+                  <p className='font-bold bg-secundair text-white text-center text-xs uppercase'>KG</p>
+                  <div className='flex p-1'>
+                     <button className='w-6 h-6 border-2 flex items-center justify-center text-denied border-denied rounded-full'>
+                        <HiOutlineMinus/>
+                     </button>
+                     <input 
+                        type="number" 
+                        placeholder='1'
+                        className=' rounded-b w-14 text-center'
+                     />
+                     <button className='w-6 h-6 border-2 flex items-center justify-center text-accept border-accept rounded-full'>
+                        <HiPlus/>
+                     </button>
+                  </div>
                </div>
-               <button className='w-6 h-6 border-2 flex items-center justify-center text-accept border-accept rounded-full'>
-                  <HiPlus/>
-               </button>
             </div>
          </main>
       </Modal>
