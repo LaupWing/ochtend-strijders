ffb07342456ca9915b97c248f3e769399447840d
LaupWing <loc_strict@hotmail.com>
2022-06-25 15:18:05 +0200

    insatlled icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,12 @@ const Home: NextPage = () => {
    return (
       <div className='w-screen h-screen bg-main'>
          <header className='w-full h-14 bg-secundair'></header>
+         
+         <button 
+            className='font-bold text-2xl w-12 h-12 bg-accept rounded-full flex justify-center items-center text-white'
+         >
+            +
+         </button>
       </div>
    )
 }
