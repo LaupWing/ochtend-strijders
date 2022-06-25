ab75ad0d8c269af9be71ea5dd5363c3c1586575c
LaupWing <loc_strict@hotmail.com>
2022-06-25 20:09:43 +0200

    Counter cmp

---
+++ src/components/AddExcercise/Counter/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { HiOutlineMinus, HiPlus } from 'react-icons/hi'
+
+const Counter = () => {
+   return (
+      <div className='flex items-end space-x-2'>
+         <div className='flex flex-col mt-auto border-2 border-highlight rounded'>
+            <p className='font-bold bg-highlight text-white text-center text-xs uppercase'>KG</p>
+            <div className='flex p-1'>
+               <button className='w-6 h-6 border-2 flex items-center justify-center text-denied border-denied rounded-full'>
+                  <HiOutlineMinus/>
+               </button>
+               <input 
+                  type="number" 
+                  placeholder='1'
+                  className=' rounded-b w-14 text-center'
+               />
+               <button className='w-6 h-6 border-2 flex items-center justify-center text-accept border-accept rounded-full'>
+                  <HiPlus/>
+               </button>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Counter
--- src/components/AddExcercise/index.tsx
+++ src/components/AddExcercise/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Modal from 'react-modal'
-import {HiOutlineMinus, HiPlus} from 'react-icons/hi'
+import Counter from './Counter'
 
 const AddExcercise = () => {
    return (
@@ -21,24 +21,7 @@ const AddExcercise = () => {
                   placeholder='Oefening'
                />
             </div>
-            <div className='flex items-end space-x-2'>
-               <div className='flex flex-col mt-auto border-2 border-highlight rounded'>
-                  <p className='font-bold bg-highlight text-white text-center text-xs uppercase'>KG</p>
-                  <div className='flex p-1'>
-                     <button className='w-6 h-6 border-2 flex items-center justify-center text-denied border-denied rounded-full'>
-                        <HiOutlineMinus/>
-                     </button>
-                     <input 
-                        type="number" 
-                        placeholder='1'
-                        className=' rounded-b w-14 text-center'
-                     />
-                     <button className='w-6 h-6 border-2 flex items-center justify-center text-accept border-accept rounded-full'>
-                        <HiPlus/>
-                     </button>
-                  </div>
-               </div>
-            </div>
+            <Counter/>
          </main>
       </Modal>
    )
