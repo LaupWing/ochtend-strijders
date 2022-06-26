34582b695e4b7e26630f3a9df5c9d393c9872c93
LaupWing <loc_strict@hotmail.com>
2022-06-26 06:52:51 +0200

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
                <Excercise key={a}/>
             ))}
          </main>
-         <AddExcercise/>
+         {/* <AddExcercise/> */}
          <button 
             className='font-bold text-2xl w-12 h-12 bg-accept rounded-full flex justify-center items-center text-white fixed bottom-5 right-5 shadow'
          >
